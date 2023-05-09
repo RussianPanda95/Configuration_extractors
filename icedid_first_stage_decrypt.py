ec312c4422324f3d40d8b81afb69eb971732951d
RussianPanda <91575783+RussianPanda95@users.noreply.github.com>
2023-05-09 17:49:00 -0400

    Add files via upload

---
+++ icedid_first_stage_decrypt.py
@@ -0,0 +1,32 @@
+# Author: RussianPanda
+# Tested on samples (samples can be found on www.unpac.me):
+# 8fc683128de2f77baddeff88b5fb427c70f9f099cd293032d780e3e06b6f947b
+# fd37c98782453214bab6484f6045b796a5a3dc7ebba9a894f6783817eef6c9c7
+# dd651c2ffe94faf59e3a3db2da56e05a1a12fcae7cd5f87881d1cb036be3ec2a
+
+import pefile
+import struct
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--file", help="path of the binary file")
+args = parser.parse_args()
+pe = pefile.PE(args.file)
+
+for s in pe.sections:
+    if s.SizeOfRawData > 256:
+        rdata_start = s.VirtualAddress + pe.OPTIONAL_HEADER.ImageBase
+        rdata_end = rdata_start + s.Misc_VirtualSize
+        rdata_data = s.get_data()
+
+bytes = bytearray()
+
+for i in range(32):
+    bytes.append(rdata_data[i] ^ rdata_data[i+64])
+
+key = rdata_data[:32].hex()
+campaign_id = struct.unpack("<I", bytes[:4])[0]
+c2 = bytes[4:].split(b'\x00')[0].decode()
+print(f"C2: {c2}")
+print(f"Campaign ID: {campaign_id}")
+print(f"Key: {key}")
