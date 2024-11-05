9ec9f8a00dce0255db435c86b3c26cc797fb3722
cccs-rs <62077998+cccs-rs@users.noreply.github.com>
2024-11-05 23:11:54 +0000

    Add rule for PoseidonStealer to only target Mach-o files

--- poseidon_config_extractor.py
+++ poseidon_config_extractor.py
@@ -97,6 +97,28 @@ class PoseidonStealer(Extractor):
     last_modified = "2024-10-16"
     sharing: str = "TLP:CLEAR"
     reference: str = "https://www.esentire.com/blog/poseidon-stealer-uses-sora-ai-lure-to-infect-macos"
+    # Rule is meant to trigger on Mach-O files
+    yara_rule: str = """
+rule ft_macho
+{
+   meta:
+      author = "Jamie Ford"
+      company = "BroEZ"
+      lastmod = "September 5 2016"
+      desc = "Signature to trigger on mach-o file format."
+
+   strings:
+      $MH_CIGAM_64 = { CF FA ED FE }
+      $MH_MAGIC_64 = { FE ED FA CF }
+      $MH_MAGIC_32 = { FE ED FA CE }
+      $MH_CIGAM_32 = { CE FA ED FE }
+      $FAT_MAGIC = { CA FE BA BE }
+      $FAT_CIGAM = { BE BA FE CA }
+
+   condition:
+      ($MH_CIGAM_64 at 0) or ($MH_MAGIC_64 at 0) or ($MH_CIGAM_32 at 0) or ($MH_MAGIC_32 at 0) or ($FAT_MAGIC at 0) or ($FAT_CIGAM at 0)
+}
+"""
 
     def run(self, stream: BinaryIO, matches: List = []) -> Optional[ExtractorModel]:
         # Reset printed_configs per each run to prevent config leaks onto other samples
